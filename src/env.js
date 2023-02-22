5ec193bb67b4d65690bc7a9df4dee6c249ef5022
Jake Overall <joverall22@gmail.com>
2023-02-22 14:26:20 -0700

    updates to use sandbox api

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'abc-towing.us.auth0.com'
-export const clientId = 'pECbyD8lsoKeI2mhD1vzWSsIwhZNuO7T'
-export const audience = 'https://abc-towing.com'
+export const domain = 'codeworksclassroom.auth0.com'
+export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const audience = 'https://codeworksclassroom.com'
--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -4,21 +4,21 @@ import { api } from "./AxiosService.js";
 class CarsService {
 
   async getCars() {
-    const res = await api.get('api/cars')
+    const res = await api.get('auth/api/cars')
     console.log('[get cars]', res.data)
     AppState.cars = res.data
     console.log(AppState.cars);
   }
 
   async createCar(carData) {
-    const res = await api.post('api/cars', carData)
+    const res = await api.post('auth/api/cars', carData)
     AppState.cars.push(res.data)
     return res.data
     // TODO something fancy with the router
   }
 
   async editCar(carData) {
-    const res = await api.put('api/cars/' + carData.id, carData)
+    const res = await api.put('auth/api/cars/' + carData.id, carData)
     let i = AppState.cars.findIndex(c => c.id == carData.id)
     if (i == -1) {
       throw new Error('umm wat!?!')
@@ -28,12 +28,12 @@ class CarsService {
 
   async getCarById(carId) {
     AppState.car = null
-    const res = await api.get('api/cars/' + carId)
+    const res = await api.get('auth/api/cars/' + carId)
     AppState.car = res.data
   }
 
   async removeCar(carId) {
-    const res = await api.delete('api/cars/' + carId)
+    const res = await api.delete('auth/api/cars/' + carId)
     let i = AppState.cars.findIndex(c => c.id == carId)
     if (i != -1) {
       AppState.cars.splice(i, 1)
